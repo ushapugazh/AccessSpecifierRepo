bd1533a6aeeece51babbdef57d3d276e8602d043
Dell - Laptop <Dell - Laptop@DESKTOP-BGOGI7G.mshome.net>
2025-05-19 17:22:20 +0530

    First Commit

---
+++ access-specifiers/src/main/java/com/core/java/A.java
@@ -0,0 +1,15 @@
+package com.core.java;
+
+public class A extends B{
+	 public static  int i=10;
+	  void m() {
+	// System.out.println(j);//ce j is not visible
+		  int a =35;
+}
+}
+
+
+class B{
+	private int j=15;
+}
+
